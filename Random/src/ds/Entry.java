d8add472ccfde3aaf002578af0fec665bca8a436
mathuvenkat <mathuvenkat@gmail.com>
2013-09-23 16:31:48 -0700

    added hashmap

---
+++ Random/src/ds/Entry.java
@@ -0,0 +1,47 @@
+package ds;
+
+public class Entry<K,V>
+{
+   K key;
+   V value;
+   Entry<K,V> next;
+
+   public Entry(K k, V v)
+   {
+     this.key = k;
+     this.value = v;
+   }
+
+   public Entry(K k, V v , Entry<K,V> next)
+   {
+     this.key = k;
+     this.value = v;
+     this.next = next;
+   }
+
+   public K getKey()
+   {
+     return this.key;
+   }
+
+   public V getValue()
+   {
+      return this.value;
+   }
+
+   public void setValue( V val)
+   {
+      this.value = val;
+   }
+
+   public void setNext(Entry<K,V> next)
+   {
+      this.next = next;
+   }
+
+   public Entry<K,V> getNext( )
+   {
+      return this.next;
+   }
+
+}
---
+++ Random/src/ds/HashMap.java
@@ -0,0 +1,62 @@
+package ds;
+
+public class HashMap<K, V>
+{
+   int DEFAULT_SIZE = 16;
+   Entry<K, V> table[];
+
+   @SuppressWarnings("unchecked")
+   public HashMap()
+   {
+      table = new Entry[DEFAULT_SIZE];
+   }
+
+   private int getIndex(K key)
+   {
+      int hashLoc = key.hashCode() % table.length;
+      return hashLoc;
+   }
+
+   public void put(K key,
+                   V value)
+   {
+      int hashLoc = getIndex(key);
+
+      if (table[hashLoc] == null) {
+         table[hashLoc] = new Entry<K, V>(key, value);
+      } else {
+
+         Entry<K, V> entry = table[hashLoc];
+         Entry<K, V> firstEntry = entry;
+
+         while (entry != null) {
+            if (entry.getKey().equals(key)) {
+               entry.setValue(value);
+               return;
+            } else {
+               entry = entry.getNext();
+            }
+         }
+
+         //key doesnt exist - insert new key
+         table[hashLoc] = new Entry<K, V>(key, value, firstEntry);
+      }
+   }
+
+   public V get(K key)
+   {
+      int hashLoc = getIndex(key);
+
+      Entry<K, V> entry = table[hashLoc];
+
+      while (entry != null) {
+         if (entry.getKey() == key) {
+            return entry.getValue();
+         } else {
+            entry = entry.getNext();
+         }
+      }
+      return null;
+   }
+
+}
